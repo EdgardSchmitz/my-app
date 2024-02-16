dfeddfd8f25f9a03c8fb3efd9f9d7245c7b3abc6
Edgard Schmitz <edgardschmitz83@gmail.com>
2024-02-16 20:52:59 -0300

    Add Participante e botões

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -2,24 +2,43 @@ import { Text, View, TextInput, TouchableOpacity} from 'react-native';
 
 import {styles} from './styles';
 
+import {Participante} from '../../components/Participante';
+
 export default function Home(){
 
     function handleParticipantAdd (){
         console.log("Você clicou aqui!");
     }
+
+    function handleParticipantRemove(name: string){
+        console.log(`Você clicou para remover ${name}`);
+    }
+
+
 return (
     <View style={styles.container}>
+
     <Text style={styles.eventName}>
     Nome do Evento</Text>
+
     <Text style={styles.eventDate}>Sexta, 09 de Fevereiro de 2024.</Text>
+
+    <View style={styles.form}>
+
     <TextInput 
     style={styles.input} 
     placeholder="Nome do Participante"
     placeholderTextColor="#6b6b6b"
     />
+
     <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
         <Text style={styles.buttonText}>+</Text>
     </TouchableOpacity>
+
+    </View>
+
+    <Participante name="Edgard" onRemove={() => handleParticipantRemove('Edgard')}/>
+    
     </View>
 )
 }
