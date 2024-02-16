a9e0ef2f2e95481d7d7268f849af5b72f1056091
Edgard Schmitz <edgardschmitz83@gmail.com>
2024-02-15 23:05:12 -0300

    add alements

--- App.js
+++ App.js
@@ -1,29 +1,9 @@
-import { Text, View, StyleSheet } from 'react-native';
+import Home from "./src/screens/home";
 
 export default function App(){
   return (
-    <View style={styles.container}>
-    <Text style={styles.eventName}>
-    Nome do Evento</Text>
-    <Text style={styles.eventDate}>Sexta, 09 de Fevereiro de 2024.</Text>
-    </View>
-  )
+    <Home />
+  );
 }
 
 
-const styles =StyleSheet.create({
-  container:{
-    flex: 1,
-    backgroundColor: '#131016',
-    padding: 24
-  }, eventName:{
-    color:'#FDFCFE',
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginTop: 48
-  },
-  eventDate:{
-    color:'#6b6b6b',
-      fontSize: 16
-  }
-});
