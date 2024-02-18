9fdc0902a1cf5c84b07c9dc82cab9cd5804e5a2f
Edgard Schmitz <edgardschmitz83@gmail.com>
2024-02-18 20:38:19 -0300

    ultimos detalhes da aula

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
+import { StatusBar } from "expo-status-bar";
+
 import Home from "./src/screens/home";
 
 export default function App(){
   return (
+    <>
+    <StatusBar
+    style="light"
+    backgroundColor="transparent"
+    translucent
+    />
     <Home />
+    </>
+    
   );
 }
 
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,18 +1,37 @@
-import { Text, View, TextInput, TouchableOpacity, ScrollView} from 'react-native';
+import React, {useState} from 'react';
+
+import { Text, View, TextInput, TouchableOpacity, FlatList, Alert} from 'react-native';
 
 import {styles} from './styles';
 
 import {Participante} from '../../components/Participante';
 
 export default function  Home(){
-    const participants = ['Edgard','Jessica', 'Willian', 'Mayla', 'Marcio', 'Canario', 'Robson', 'Agatha', 'Layse', 'Vinicius', 'Rubens Meneguetti', 'Roberto Marques'];
+
+    const [participants, setParticipants] = useState<string[]>([]);
+    const [participantName, setParticipantName] = useState('');
 
     function handleParticipantAdd (){
-        console.log("Você clicou aqui!");
+            if(participants.includes(participantName)){
+            return Alert.alert("Participante Existe", "Já existe um participante na lista com esse nome.")
+        }
+
+        setParticipants(prevState => [...prevState, participantName]);
+        setParticipantName('');
     }
 
     function handleParticipantRemove(name: string){
-        console.log(`Você clicou para remover ${name}`);
+
+        Alert.alert("Remover", `Remover participante ${name} ?`, [
+            {
+                text: 'Sim',
+                onPress: () => setParticipants(prevState => prevState.filter(participant => participant !== name))
+            },
+            {
+                text: 'Não',
+                style: 'cancel'
+            }
+        ]);
     }
 
 
@@ -30,6 +49,8 @@ return (
     style={styles.input} 
     placeholder="Nome do Participante"
     placeholderTextColor="#6b6b6b"
+    onChangeText={setParticipantName}
+    value={participantName}
     />
 
     <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
@@ -40,18 +61,22 @@ return (
 
     </View>
 
-    <ScrollView showsVerticalScrollIndicator={false}>
-    {
-        participants.map(participant => (
-            <Participante 
-            key={participant}
-            name={participant} 
-            onRemove={() => handleParticipantRemove("Edgard")}/>
-        ))
-    }
-
-</ScrollView>
-    
+    <FlatList
+    data={participants}
+    keyExtractor={item => item}
+    renderItem={({ item }) => (
+        <Participante 
+        key={item}
+        name={item} 
+        onRemove={() => handleParticipantRemove(item)}
+        />
+    )}
+    showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() =>
+        <Text style={styles.listEmptyText}>
+            NInguém chegou no evento ainda? Adicione participantes a sua lista de presença.
+        </Text>}
+    />
     </View>
 )
 }
